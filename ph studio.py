1a93bdc7394849230fe1a7b36553c0d8323dd330
taehoon1234 <122426526+taehoon1234@users.noreply.github.com>
2023-02-16 02:31:04 +0900

    Add files via upload
    파이썬 2주차 핑퐁게임 만들기 
     파이썬의 막대기를 움직이게 하기 위하여 각각 방향별로 함수를 선언했다 그리고 막대기가 범위 밖으로 넘어 가는걸 방지하기 위해  200,-200으로 선언했다. 그리고 볼이라는 변수를 선언하고 shape을 통해 모양은circle로 한다 speed는 빠르게 하기위해 0으로 하고 색깔은 흰색으로 한다

--- ph studio.py
+++ ph studio.py
@@ -2,6 +2,16 @@ import turtle as t
 import random 
 import time
 
+def right():
+    if player.xcor() < 200:
+        player.forward(10)
+
+def left():
+    if player.xcor() > -200:
+        player.backward(10)
+
+
+
 t.bgcolor("lightblue")
 t.setup(500,700)
 
@@ -11,3 +21,22 @@ player.shapesize(1,5)
 player.up()
 player.speed(0)
 player.goto(0,-270)
+
+
+# ball
+ball= t.Turtle()
+ball.shape("circle")
+ball.shapesize(1.3)
+ball.up()
+ball.speed(0)
+ball.color("white")
+
+
+
+
+
+t.listen()
+t.onkeypress(right,"Right")
+t.onkeypress(left,"Left")
+
+
